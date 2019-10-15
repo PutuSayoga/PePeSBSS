71dc015aa273b6e1800096f1155b1e4caf4e0495
sayoga putu gede <putugedesayoga@gmail.com>
2019-10-16 00:04:29 +0700

    #1 Initial commit. Add web app .NET Core 3.0 with MVC template(-Controller and Views from 'Home'), libman, gitignore (+ignoring everything inside /wwwroot/lib/)

---
+++ PresentationLayer.Web.MvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PresentationLayer.Web.MvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
